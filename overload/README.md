87b802570b28ce08fcfad0f278a81f1de9164bfc
Static-Papaya <282547634@qq.com>
2023-09-24 17:36:28 +0800

    modified:   overload/README.md 	modified:   overload/src/overload.js

--- overload/README.md
+++ overload/README.md
@@ -1,17 +1,24 @@
 # 在JavaScript中使用重载   
-### Version 0.2.2
+### Version 0.3.0
 ---  
 
-## 更新  
-1. 废弃 自定义对象类型时通过对象构造器名称来指定重载。[详情](#t_1)
+#### 2023.9.23 0.3.0 更新 
+1. 新增`typeNone`钩子。[详情](#type_none_add)  
+2. `autoLength`钩子支持一次性定义多个类型。[详情](#auto_length_update)  
+3. 修复了jsdoc部分错误。  
+
+#### 2023.9.20 0.2.2 更新 
+1. 修复Bug.  
+
+#### 2023.9.12 0.2.1 更新  
+1. 废弃 自定义对象类型时通过对象构造器名称来指定重载。[详情](#overload_define_discard)
 2. 更改 类型'Function' 更改为 'function'; undefined 移除'undefined'; Array 移除 'Array'.
 3. 新增 类型Object , Promise, RegExp 等。
 
-## 引入  
-``` js
-    import { overload } from './overload.js';
-```
-## 如何使用  
+---
+## 如何使用
+#### 基础功能
+
 ```
   overload(f): overload; // 无参函数重载
   overload(types..., f): overload; // 有参函数重载
@@ -19,7 +26,6 @@
 ```  
 
 &emsp;&emsp;overload需要输入一组类型名称，它们分别对应重载函数形参列表，类型名指定后要在末尾设置重载函数。一组重载定义完成后可以接着后面定义新的一组重载，格式也完全一样。当所有的重载都定义完成时需要不传任何值执行一次，该过程会生成一个聚合函数，当我们调用聚合函数时会根据输入参数的类型自动匹配运行其内部的重载函数。  
-
 
 ```js
     import { overload } from './overload.js';
@@ -35,7 +41,7 @@
 ```  
 
 &emsp;&emsp;如果我们在指定参数类型时需要用到自定义对象，您可以像下面这样进行定义。  
-<h5 id="t_1">
+<h5 id="overload_define_discard">
 注：在以往的版本中，您可以通过传入对象名来定义重载参数，但由于这种方式存在很多不确定性，面对不同模块中同名的构造器时就会产生歧义，因此目前已不再支持这种方式定义参数。
 </h5>
 
@@ -59,30 +65,68 @@
     // sub(new PNumber(1), new PNumber(2)); -1
 ```  
 
-&emsp;&emsp;需要注意的是有时候我们很容易将Number、String这样函数当成类型直接传入，overload内部会将他们解析为Function构造器，当被错误引导的我们调用它们时一个错误就产生了。  
+&emsp;&emsp;需要注意的是有时候我们很容易将Number、String这样函数误当成类型直接传入，~~overload内部会将他们解析为Function构造器，当被错误引导的我们调用它们时一个错误就产生了。~~ (请参考对象类型内部机制) 请使用'number', 'string' 等样式字符串来指定基本数据类型，您也可以参考文档最后的类型表。
 
-&emsp;&emsp;上面这些可以明确知道每个类型参数的个数，这种类型的重载我称它匹配重载。有时候一些参数可能不固定，面对这种情况我们将使用autoLength来包装参数类型此时重载就变成了通项重载。只要参数列表中有一个使用了autoLength包装，那么这一条重载都将变为通项重载。  
+<details>
+    <summary>对象类型内部机制</summary>
+    <p>&emsp;&emsp;以前的版本中，在指定对象类型重载参数的时候，overload会直接使用xxx.name属性(字符串类型)作为key保存在映射表中，因此在我们调用聚合函数的时候如果传入了一个实例化的对象，就会调用xxx.constructor.name来和映射表中的各种key进行匹配，对于没有constructor.name的情况会使用'object'字符串和映射表匹配。因此大多数情况下，实例化对象的constructor和重载定义的构造器名称只要一致就会发生匹配，对于不同模块如果出现同名类型作为重载参数时，就会出现指定不明。</p>
+    <p>&emsp;&emsp;现在版本，引入WeakMap(不兼容的情况下使用链表结构)通过储存原对象的弱引用实现参数类型的映射，在重载定义阶段，overload会对传入的值进行一次typeof，所得结果如果为'string'则会使用传入的值作为永久映射，其余的情况如果传入的值为`undefined`、`null`、`0`则会使用typeof结果作为永久映射，否则使用传入值(类、构造函数)的弱引用放入WeakMap作为映射。调用聚合函数传入实例化对象时，会通过xxx.constructor到WeakMap中寻找映射值。</p>
+    <p>&emsp;&emsp;在兼容模式下，使用链表代替WeakMap，因此需要注意在定义阶段，传入的类、构造器都会被作为强引用类型保存下来。</p>
+</details>
+
+#### 使用 autoLength 钩子
+
+&emsp;&emsp;上面这些可以明确知道每种类型参数的个数，这种类型的重载我称它匹配重载。有时候一些参数可能不固定，面对这种情况我们将使用autoLength来包装参数类型此时重载就变成了通项重载。只要参数列表中有一个使用了autoLength包装，那么这一条重载都将变为通项重载。  
+
+<p id="auto_length_update"></p>  
 
 ``` js
     import { overload, autoLength } from './overload.js';
 
+    // 纯数字
     const add = overload(autoLength('number'), function(...values) {
         return values.reduce((total, v) => total + v, 0);
+    })
+    // 字符串 + 数字
+    (autoLength('string'), autoLength('number'), function(...values) {
+        return values.reduce((pre, v) => pre + v, '');
+    })
+    // 数字 + 字符串。这是一次指定多个类型的写法
+    (autoLength('number', 'string'), function(...values) {
+        return values.reduce((pre, v) => pre + Number(v), 0);
     })();
 
     add(1, 2, 3, 4); // 10
-    add(5, 6, 7); // 18
-
-    const sub = overload(autoLength('string'), autoLength('number'), function(...values) {
-        return values.reduce((total, v) => total - v, 0);
-    })();
-
-    sub('1', '2', 3, 4); // -10
-    sub('5', 6, 7); // -18
+    add('1', '2', 3, 4); // '1234'
+    add(6, 7, '5'); // 18
 ```
 
-## 细节  
+**注意:** `autoLength`仅用于重载定义阶段，执行阶段直接使用会报错。同时请勿滥用通项重载，复杂的通项重载除了让代码辨识度降低造成逻辑混乱，还可能造成重载覆盖。  
 
+<h4 id="type_none_add">使用 typeNone 钩子</h4>
+&emsp;&emsp;假设存在这种情况，可执行函数形参是一个可选参数，如果我们仅传入一个值，如下。  
+
+``` js
+    import { overload } from './overload.js';
+    const add = overload('number', 'number', (a = 0, b = 1) => a + b)();
+
+    add(1); // error
+```  
+
+&emsp;&emsp;那么毫无疑问执行聚合函数的时候会报错。但我们又希望在传入一个值的时候触发该函数，该怎么解决呢?`typeNone`钩子就用于解决这一问题，它就像一个带有类型的undefined，仅用于匹配形参类型但不表示任何值。使用方式如下。  
+
+``` js
+    import { overload, typeNone } from './overload.js';
+    const add = overload('number', 'number', (a = 0, b = 1) => a + b)();
+
+    add(typeNone('number'), 5); // 0 + 5 = 5
+    add(3, typeNone('number')); // 3 + 1 = 4
+    add(typeNone('number'), typeNone('number')); // 0 + 1 = 1
+```
+
+**注意:** `typeNone`仅用于聚合函数执行阶段，在重载定义阶段直接使用则会报错。
+
+## 细节  
 &emsp;&emsp;匹配重载即每一个类型都匹配上才会执行相应的重载函数。而通项重载则是一个不确定长度的重载，一定范围内只要输入的参数类型不发生改变那么就会匹配，当所有条件满足的时候执行相应的重载函数。  
 
 &emsp;&emsp;当我们调用聚合函数的时候，overload会优先按照匹配重载的规则寻找符合条件的函数并执行。如果没有找到就会按照通项重载的规则查找相应的重载函数并执行。如果前两个都没有找到就会报错。  
@@ -90,6 +134,8 @@
 &emsp;&emsp;通过上面的解释我们会发现优先级：匹配重载 > 通项重载。没错，所以我们在通过它们进行编码时应当注意如果可以确定每个参数长度的尽量用匹配重载，不当的使用通项重载会产生冗余的运算。  
 
 &emsp;&emsp;当然有一种情形除外，如果我们输入的参数个数大于所有匹配重载最长的参数个数，那么overload会直接按照通项重载来寻找重载函数。
+
+
 
 ## 如果不知道类型怎么办?  
 
@@ -112,4 +158,3 @@
 | RegExp         | RegExp     |                      `/^[123].txt$/` |
 | Object         | Object     |                                 `{}` |
 | Promise        | Promise    |                      `new Promise()` |
-
--- overload/src/overload.js
+++ overload/src/overload.js
@@ -2,13 +2,13 @@
  * @file Overload in JavaScript
  * @author Static-Papaya
  * @link https://github.com/Static-Papaya
- * @version 0.2.2
+ * @version 0.3.0
  * @license MIT
  */
 
-/*
- * 全局字面量
- */
+/* ==============
+**   全局字面量
+*/// ============
 /** ResultMap 最新下标 */
 const $RESULT_MAP_INDEX = Symbol('index');
 const $RESULT_MAP_DEFAULT = Symbol('default');
@@ -17,6 +17,17 @@ const $RESULT_MAP_MAX_LENGTH = Symbol('max-args-length');
 /** undefined */
 const $UNDEFINED = void 0;
 
+/* ==============
+**   语义字面量
+*/// ============
+/** 自适应语义 */
+const $SEMANTICS_ADJUST = 'semantics-adjust';
+/** 类型无值语义 */
+const $SEMANTICS_TYPE_NONE = 'semantics-type-none';
+
+/* ==============
+**   全局函数
+*/// ============
 /**
  * 获取全局对象
  * @returns {globalThis}
@@ -36,7 +47,13 @@ function global() {
     }
 }
 
-const _ObjectResultMap = global().WeakMap || class {
+/* ==============
+**   全局对象
+*/// ============
+/**
+ * ResultMap所使用的储存方式，如果不兼容WeakMap就会使用链表实现。
+ */
+const _ObjectResultMap = global().WeakMap || (() => {
     /** 
      * 链表子节点
      * @private
@@ -45,18 +62,23 @@ const _ObjectResultMap = global().WeakMap || class {
      * @property {any} instanceKey 相当于WeakMap的key
      * @property {any} value 相当于WeakMap的value
     */
-
-    /** @private @type {_ObjectResultMapNode} */
-    _next = $UNDEFINED;
-    /** @private @type {_ObjectResultMapNode} */
-    _last = this;
+    /**
+     * 使用链表代替WeakMap不兼容的情况
+     * @constructor
+     */
+    const ReplaceMap = function () {
+        /** @private @type {_ObjectResultMapNode} */
+        this._next = $UNDEFINED;
+        /** @private @type {_ObjectResultMapNode} */
+        this._last = this;
+    }
 
     /**
      * 写入
      * @property {any} instanceKey 相当于WeakMap的key
      * @property {any} value 相当于WeakMap的value
      */
-    set(instanceKey, value) {
+    ReplaceMap.prototype.set = function (instanceKey, value) {
         let target = this[instanceKey];
 
         if (target === $UNDEFINED) {
@@ -71,7 +93,7 @@ const _ObjectResultMap = global().WeakMap || class {
      * @property {any} instanceKey 相当于WeakMap的key
      * @returns {_ObjectResultMapNode}
      */
-    get(instanceKey) {
+    ReplaceMap.prototype.get = function (instanceKey) {
         let target = this._next;
 
         while (target !== $UNDEFINED) {
@@ -87,30 +109,34 @@ const _ObjectResultMap = global().WeakMap || class {
      * @property {any} instanceKey 相当于WeakMap的key
      * @returns {boolean}
      */
-    has(instanceKey) {
+    ReplaceMap.prototype.has = function (instanceKey) {
         return !!this.get(instanceKey);
     }
 
     /**
      * 冻结
      */
-    freeze() {
+    ReplaceMap.prototype.freeze = function () {
         let node = this._next;
         while (node) {
             Object.freeze(node);
             node = node._next;
         }
     }
-};
+
+    return ReplaceMap;
+})();
 
 /**
- * 用于标识可变参数
+ * 语义 - 主要用于一些特殊标识，不同的语义在定义或执行阶段执行不同处理，比如autoLength等。
+ * 语义处理的意义是不同类型语义，在定义或执行阶段被特殊处理，用于计算参数的类型，如果在执行阶段可能还会改变传入形参的值。
+ * @constructor
+ * @param {string} type 语义类型
+ * @param {Array<any>} typeList 参数类型
  */
-class AutoLengthType {
-    constructor(type) {
-        this.type = 'adjust';
-        this.typeList = type;
-    }
+function Semantics(type, typeList) {
+    this.type = type;
+    this.typeList = typeList;
 }
 
 /**
@@ -133,10 +159,94 @@ const ResultMap = function (defaultKey = '_') {
     return map;
 }
 
+/* ==============
+**   语义钩子
+*/// ============
+/** 
+ * 将类型包装成可变参数类型。(定义阶段)
+ * 在定义重载函数的时候，通过该函数修饰的类型会让该形参的个数变得可以变化。
+ * @param {'number'|'string'|'boolean'|'function'|'symbol'|'bigint'|Array|Promise} targetType 参数类型
+ * @returns {Semantics} 可变参数类型语义
+ * @example
+ * ----------
+ * const add = overload(autoLength('number'), function (...numbers) {
+ *    let count = 0;
+ *    numbers.map(value => count += value);
+ *    return count;
+ * })()
+ * 
+ * add(1, 2, 3, 4, 5, 6, 7, 8, 9); // 45
+ */
+function autoLength(...targetType) {
+    const auto = new Semantics($SEMANTICS_ADJUST, targetType);
+    Object.freeze(auto);
+    return auto;
+}
+
+/** 
+ * 类型无值(聚合函数使用时)
+ * 在聚合函数使用时，如果您希望传入一个带有附属类型的undefined值用于占位，该语义函数正好可以满足这一条件。
+ * @param {'number'|'string'|'boolean'|'function'|'symbol'|'bigint'|Array|Promise} targetType 参数类型
+ * @returns {Semantics} 类型无值语义
+ * @example
+ * ----------
+ * const add = overload('number', 'number', (a = 2, b) => a + b)()
+ * add(typeNone('number'), 1); // 3
+*/
+function typeNone(targetType) {
+    const noneValue = new Semantics($SEMANTICS_TYPE_NONE, [targetType]);
+    return noneValue;
+}
+
+/* ==============
+**   语义处理
+*/// ============
+/**
+ * 定义阶段语义处理
+ * @param {Semantics} semantics 语义
+ * @param {0|1} isPatch 是否使用匹配映射
+ * @returns {[type: any, isPatch: number]} 处理后的类型以及是否使用匹配映射
+ */
+function handleSemanticsOnDefine(semantics, isPatch) {
+    let type;
+
+    switch (semantics.type) {
+        case $SEMANTICS_ADJUST: // 自适应语义
+            isPatch = 0;
+            type = semantics.typeList; break;
+        default:
+            throw new Error('定义阶段未支持的语义');
+    }
+
+    return [type, isPatch];
+}
+
+/**
+ * 执行阶段语义处理
+ * @param {Semantics} semantics 语义
+ * @param {any} param 原有参数
+ * @return {[type: any, param: any]} 类型, 处理后的参数
+ */
+function handleSemanticsOnCall(semantics, param) {
+    let type;
+
+    switch (semantics.type) {
+        case $SEMANTICS_TYPE_NONE: // 类型无值语义
+            type = semantics.typeList;
+            param = $UNDEFINED; break;
+        default: throw new Error('执行了一个未支持的语义');
+    }
+
+    return [type, param];
+}
+
+/* ==============
+**   处理函数
+*/// ============
 /**
  * 向ResultMap中添加映射.
  * @param {ResultMap} map 类型字段映射表 
- * @param {string} key 键名
+ * @param {string|any} key 键名
  * @return {number} 映射值
  */
 function setMap(map, key) {
@@ -215,7 +325,10 @@ function formatResultToAuto(mapping) {
 /**
  * 在重载定义阶段时获取输入的类型.
  * @param {any} type 定义阶段输入的类型
- * @returns {string | any} 类型同一化
+ * @returns {string | any} 类型统一化。
+ * 如果输入的typeof = 'string' 输出typeof;
+ * 如果输入 = undefined / 0 输出typeof;
+ * 其他情况输出输入。
  */
 function getTypeOnDefine(type) {
     let resultType = typeof type;
@@ -232,22 +345,31 @@ function getTypeOnDefine(type) {
  */
 function setOverloadOnDefine(list, templateList, map, options) {
     const loaderFunction = options.pop();
-    let definedType = 1, types;
+    let definedType = 1, types = [];
 
-    types = options.map(type => {
-        let resultType;
-
-        if (type instanceof AutoLengthType === false) {
+    for (let i = 0, max = options.length, resultType, type; i < max; ++i) {
+        type = options[i];
+        if (type instanceof Semantics === false) {
+            // 获取输入的类型
             resultType = getTypeOnDefine(type);
+            types.push(resultType);
+            // 写入到映射表
+            setMap(map, resultType);
         }
         else {
-            definedType = 0;
-            resultType = type.typeList;
+            // 执行定义阶段语义处理
+            [resultType, definedType] = handleSemanticsOnDefine(type, definedType);
+
+            // 对每一个类型进行处理
+            for (let resultTypeItem of resultType) {
+                // 获取输入的类型
+                resultTypeItem = getTypeOnDefine(resultTypeItem);
+                types.push(resultTypeItem);
+                // 写入到映射表
+                setMap(map, resultTypeItem);
+            }
         }
-        // 写入到映射表
-        setMap(map, resultType);
-        return resultType;
-    });
+    }
 
     if (definedType) {
         list[calculateResult(map, '_', types)] = loaderFunction;
@@ -262,32 +384,14 @@ function setOverloadOnDefine(list, templateList, map, options) {
     }
 }
 
-/** 
- * 将类型包装成可变参数类型。
- * 在定义重载函数的时候，通过该函数修饰的类型会让该形参的个数变得可以变化。
- * @param {'string'|'number'|'boolean'|'Array'|'Function'|'bigint'|'object'|any} targetType 参数类型
- * @returns {AutoLengthType} 可变参数类型
- * @example
- * ----------
- * let add = overload(autoLength('number'), function (...numbers) {
- *    let count = 0;
- *    numbers.map(value => count += value);
- *    return count;
- * })()
- * 
- * add(1, 2, 3, 4, 5, 6, 7, 8, 9); // 45
- */
-function autoLength(targetType) {
-    let auto = new AutoLengthType(getTypeOnDefine(targetType));
-    Object.freeze(auto);
-    return auto;
-}
-
+/* ==========
+**   主函数
+*/// ========
 /**
  * 构造一个函数的重载，依次输入重载函数的类型，然后是执行函数。
  * 该函数使用柯里化执行，可输入多次重载。最后完成重载后需要以空的参数执行一次。
  * 详细可以见下方例子。
- * @param  {'string'|'number'|'boolean'|'Array'|'Function'|'bigint'|'object'|any} options 重载项配置，依次输入参数类型，最后一个为该重载执行函数。
+ * @param {'number'|'string'|'boolean'|'function'|'symbol'|'bigint'|Array|Promise} options 参数类型, 最后一个参数必须是函数
  * @returns {function} 构造函数的重载/统一执行入口函数
  * @example 
  * ----------
@@ -323,16 +427,28 @@ function overload(...options) {
         Object.freeze(argumentsList);
         Object.freeze(templateList);
 
-        /* 返回适配函数 */
+        /* 返回聚合函数 */
         return function (...args) {
             const paramTypes = [];
-            let type, select;
+            let type, select, index = 0;
 
             for (const param of arguments) {
                 type = typeof param;
-                paramTypes.push(
-                    type !== 'object' ? type : param && param.constructor || type
-                );
+
+                if (type !== 'object' || !(param && param.constructor)) {
+                    paramTypes.push(type);
+                }
+                else if (param instanceof Semantics === false) {
+                    paramTypes.push(param.constructor);
+                }
+                else {
+                    [type, args[index]] = handleSemanticsOnCall(param, args[index]);
+
+                    // 类型写入
+                    paramTypes.push(type[0]);
+                }
+
+                ++index;
             }
 
             if (args.length <= typesMap[$RESULT_MAP_MAX_LENGTH]) {
@@ -365,5 +481,6 @@ function overload(...options) {
 
 export {
     overload,
-    autoLength
+    autoLength,
+    typeNone
 }
